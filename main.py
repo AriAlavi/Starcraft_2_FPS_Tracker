9a3ea678a57fcf61bf12b47b654b4a0a7199038d
AriAlavi <arianalavi1@gmail.com>
2021-02-17 20:21:28 -0800

    Initial Upload

---
+++ main.py
@@ -0,0 +1,141 @@
+
+from screenReader import GetTimeProcess, GetFpsProcess
+
+from PIL import Image
+import PIL.ImageOps 
+import pytesseract
+
+import multiprocessing
+import csv
+import time
+import os
+
+
+SIMULATION_SPEED = 1.4 # Slower = .6, Slow = .8, Normal = 1, Fast = 1.2, Faster = 1.4
+
+
+
+
+
+def GetFPS(FPS_RECT, RESOURCE_STALL_RECT, save=False):
+    def GetFPSInterior(Rect, save=False):
+        fpsImage = getRectAsImage(Rect)
+        fpsImage = PIL.ImageOps.grayscale(fpsImage) 
+        if save:
+            fpsImage.save("{}fps.png".format(str(Rect)))
+        asString = pytesseract.image_to_string(fpsImage)
+        lastNum = False
+        stringWithNumsOnly = ""
+        for char in asString:
+            try:
+                int(char)
+            except:
+                if lastNum:
+                    break
+                else:
+                    continue
+            lastNum = True
+            stringWithNumsOnly += char
+        if len(stringWithNumsOnly) > 0:
+            return int(stringWithNumsOnly)
+    if save:
+        time.sleep(2)
+    return GetFPSInterior(FPS_RECT, save) or GetFPSInterior(RESOURCE_STALL_RECT, save)
+
+class TimeGetter():
+
+    TIME_DIFFERENCE_TOLERANCE = 30
+
+    lastTime = None
+    lastTimeTime = None
+
+    def __init__(self):
+        self.communicator = multiprocessing.Manager().list([None,])
+        self.killer = multiprocessing.Manager().Queue()
+        self.updater = multiprocessing.Process(target=GetTimeProcess, args=(6, .1, self.communicator, self.killer))
+        self.updater.start()
+
+    def Get(self, useFallback):
+        asTime = self.communicator[0]
+        # print("Updater time:", asTime)
+        fallbackTime = self._Fallback()        
+        if not asTime:
+            return fallbackTime
+        if not (fallbackTime - self.TIME_DIFFERENCE_TOLERANCE <= asTime <= fallbackTime + self.TIME_DIFFERENCE_TOLERANCE):
+            print("Got time is {} but it's too far from the expected time of {}".format(asTime, fallbackTime))
+            if useFallback:
+                return fallbackTime
+        self._NewFallback(asTime)
+        return asTime
+
+
+    def _GetTime(self) -> int:
+        return int(time.perf_counter() * SIMULATION_SPEED)
+
+    def _NewFallback(self, newTime):
+        self.lastTime = newTime
+        self.lastTimeTime = self._GetTime()
+    def _Fallback(self):
+        if self.lastTime:
+            return self.lastTime + self._GetTime() - self.lastTimeTime
+        return self._GetTime()
+
+    def __del__(self):
+        try:
+            self.killer.put(True)
+        except AttributeError:
+            pass
+
+class FPSGetter():
+    def __init__(self):
+        self.communicator = multiprocessing.Manager().list([None,])
+        self.killer = multiprocessing.Manager().Queue()
+        self.updater = multiprocessing.Process(target=GetFpsProcess, args=(6, .1, self.communicator, self.killer))
+        self.updater.start()
+    def Get(self):
+        return self.communicator[0]
+
+
+
+def getUniqueSqliteFileName():
+    def createName(i):
+        basename = "DATA"
+        return "{}_{}.csv".format(basename, str(i).zfill(3))
+    incrementor = 0
+    while createName(incrementor) in os.listdir():
+        incrementor += 1
+    return createName(incrementor)
+
+
+
+
+# GetFPS(FPS_AREA, FPS_AREA_RESOURCE_STALLS, True)
+def main(writer):
+    timeGetter = TimeGetter()
+    fpsGetter = FPSGetter()
+    lastTime = None
+    while True:
+        gotTime = timeGetter.Get(True)
+        time.sleep(.5)
+        # print(time)
+        if gotTime and gotTime != lastTime:
+            if lastTime and gotTime < lastTime:
+                print("Got {} but last time was {}. Cannot go back in time.".format(gotTime, lastTime))
+                continue
+            lastTime = gotTime
+            fps = fpsGetter.Get()
+            if fps:
+                writer.writerow([gotTime, fps])
+                print([gotTime, fps])
+            
+
+if __name__ == "__main__":
+    csvfile = open(getUniqueSqliteFileName(), "w", newline="")
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(["Time (seconds)", "FPS"])
+    try:
+        main(writer)
+    finally:
+        csvfile.close()
+
+
---
+++ screenReader.py
@@ -0,0 +1,196 @@
+import pyautogui
+import time
+from multiprocessing import Pool, Process
+from collections import OrderedDict
+
+# timeRegion = [350, 1035, 460, 1080]
+timeRegion = [350, 1035, 140, 40] # left, top, width, height
+fpsRegion = [2100, 950, 400, 150]
+
+TIME_IMAGE_MAP = {
+    "0" : "images/time0.png",
+    "1" : "images/time1.png",
+    "2" : "images/time2.png",
+    "3" : "images/time3.png",
+    "4" : "images/time4.png",
+    "5" : "images/time5.png",
+    "6" : "images/time6.png",
+    "7" : "images/time7.png",
+    "8" : "images/time8.png",
+    "9" : "images/time9.png",
+    ":" : "images/timeColon.png"
+}
+
+FPS_IMAGE_MAP = {
+    "0" : "images/fps0.png",
+    "1" : "images/fps1.png",
+    "2" : "images/fps2.png",
+    "3" : "images/fps3.png",
+    "4" : "images/fps4.png",
+    "5" : "images/fps5.png",
+    "6" : "images/fps6.png",
+    "7" : "images/fps7.png",
+    "8" : "images/fps8.png",
+    "9" : "images/fps9.png",
+}
+
+FPS_IMAGE = "images/fps.png"
+
+class FPSLevelFinder():
+    lastResult = None
+    topRange = 5
+    height = 30
+    def __init__(self):
+        ...
+    def Get(self) -> int:
+        foundLevel = pyautogui.locateOnScreen(FPS_IMAGE, grayscale=True, region=fpsRegion, confidence=.91)
+        if not foundLevel:
+            return self.lastResult
+        asTop = foundLevel.top - self.topRange
+        asRegion = [x for x in fpsRegion]
+        asRegion[1] = asTop
+        asRegion[2] = asRegion[2]
+        asRegion[3] = self.height
+        self._SetLast(asRegion)
+        return asRegion
+
+    def _SetLast(self, newResult):
+        self.lastResult = newResult
+
+
+def FindFPSNumber(argTup):
+    givenNumber, givenRegion = argTup
+    return (givenNumber, list(pyautogui.locateAllOnScreen(FPS_IMAGE_MAP[givenNumber], grayscale=True, region=givenRegion, confidence=.91)))
+
+def FindNumber(givenNumber):
+    return (givenNumber, list(pyautogui.locateAllOnScreen(TIME_IMAGE_MAP[givenNumber], grayscale=True, region=timeRegion, confidence=.91)))
+
+
+class FPSFinder():
+    def __init__(self, threads):
+        self.threads = threads
+        self.fpsLevelFinder = FPSLevelFinder()
+        self.fpsLevel = None
+        self.pool = Pool(self.threads)
+        self.MAX_BELIVEABLE_FPS = 400
+    def Get(self) -> int:
+        self.UpdateFPSLevel()
+        if not self.fpsLevel:
+            return None
+        results = self.pool.map(FindFPSNumber, [(x, self.fpsLevel) for x in FPS_IMAGE_MAP.keys()])
+        givenString = ArrangeResults(results)
+        if givenString:
+            asInt = int(givenString)
+            if asInt < self.MAX_BELIVEABLE_FPS:
+                return int(givenString)
+        return None
+    def UpdateFPSLevel(self):
+        self.fpsLevel = self.fpsLevelFinder.Get()
+
+def ArrangeResults(givenResults):
+    actualResults = {}
+    for result in givenResults:
+        if len(result[1]) > 0:
+            for foundInstance in result[1]:
+                actualResults[foundInstance.left] = result[0]
+    givenString = ""
+    actualResults = OrderedDict(sorted(actualResults.items()))
+    for x in actualResults.values():
+        givenString += x
+    # print(actualResults.keys())
+    return givenString
+
+class Buffer():
+    def __init__(self, length):
+        self.length = length
+        self.asList = []
+        self.index = -1
+    def add(self, elem):
+        if len(self.asList) == self.length:
+            self.index += 1
+            if self.index >= self.length:
+                self.index = 0
+            self.asList[self.index] = elem
+        else:
+            self.asList.append(elem)
+
+    def get(self):
+        return self.asList
+    
+class TimeUpdater():
+    def __init__(self, threads):
+        from multiprocessing import Pool
+        assert isinstance(threads, int) and threads > 0
+        self.threads = threads
+        self.pool = Pool(self.threads)
+        self.time = None
+        self._closed = False
+        self.ready = True
+        self.lastSecondGot = Buffer(3)
+
+    def getTime(self):
+        return self.time
+
+    def run(self):
+        while not self.ready:
+            print("Waiting, not yet ready")
+            time.sleep(.01)
+        try:
+            results = ArrangeResults(self.pool.map(FindNumber, TIME_IMAGE_MAP.keys()))[::-1].split(":")
+            results = [int(x[::-1]) for x in results]
+            # print("RESULTS:", results)
+        except:
+            self.time = None
+            return
+        totalTime = 0
+        multiplier = 1
+        for r in results:
+            totalTime += multiplier * r
+            multiplier *= 60
+        self.time = totalTime
+
+    def __del__(self):
+        try:
+            self.pool.close()
+        except AttributeError:
+            pass
+        self._closed = True
+
+
+
+def GetTimeProcess(threads, sleepTime, intPipe, killingPipe):
+    pyautogui.screenshot(region=timeRegion).save("timeRegion.png")
+    updater = TimeUpdater(threads)
+    while True:
+        updater.run()
+        intPipe[0] = updater.getTime()
+        time.sleep(sleepTime)
+        if killingPipe.qsize() > 0:
+            del updater
+            return
+
+def GetFpsProcess(threads, sleepTime, intPipe, killingPipe):
+    pyautogui.screenshot(region=timeRegion).save("fpsRegion.png")
+    updater = FPSFinder(threads)
+    while True:
+        result = updater.Get()
+        intPipe[0] = result
+        time.sleep(sleepTime)
+        if killingPipe.qsize() > 0:
+            del updater
+            return
+
+
+# def main():
+    # g = FPSFinder(6)
+    # time.sleep(2)
+    # for i in range(1, 10):
+    #     r = g.Get()
+    #     pyautogui.screenshot().save("{}.png".format(r))
+    #     time.sleep(1)
+    
+    # pyautogui.screenshot(region=timeRegion).save("timeRegion.png")
+    # TimeUpdater(6, .1)
+
+# if __name__ == "__main__":
+#     main()
